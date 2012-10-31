b8570e318390d562a81d6bcd30d5beda1b2fe830
Mako <jlauve@rsmw.net>
2012-10-31 17:38:08 -0400

    Output helpful message to the last line

--- wry.c
+++ wry.c
@@ -3,14 +3,25 @@
 #include <string.h>
 
 int g_lines, g_columns;
+void minibufmsg(char *s);
 
 int main(int argc, char **argv) {
     initscr();
     getmaxyx(stdscr, g_lines, g_columns);
+    minibufmsg("Press any key to quit.");
     printw("LINES=%d\nCOLS=%d\n", g_lines, g_columns);
     refresh();
     getch();
     endwin();
     return 0;
+}
+
+void minibufmsg(char *s) {
+    int cy, cx;
+    int y, x;
+    getyx(stdscr, cy, cx);
+    getmaxyx(stdscr, y, x);
+    mvprintw(y - 1, 0, "%s", s);
+    move(cy, cx);
 }
 
