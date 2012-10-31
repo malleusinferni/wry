5ba57337137f670d9c4bba070c30c522476eb1ba
Mako <jlauve@rsmw.net>
2012-10-31 19:03:03 -0400

    Store character count in document struct

--- wry.c
+++ wry.c
@@ -5,20 +5,24 @@
 #define DOC_SIZE (2 << 15)
 #define RULER_SIZE 80
 
-char *document;
+struct {
+    char *text;
+    size_t char_count;
+} document;
 
 int g_lines, g_columns;
 void minibufmsg(char *s);
 
 int main(int argc, char **argv) {
-    document = malloc(DOC_SIZE);
-    document[0] = '\0';
+    document.text = malloc(DOC_SIZE);
+    document.text[0] = '\0';
+    document.char_count = 0;
 
     initscr();
     getmaxyx(stdscr, g_lines, g_columns);
     minibufmsg("Press any key to quit.");
 
-    printw("%s\n", document);
+    printw("%s\n", document.text);
 
     refresh();
     getch();
@@ -30,7 +34,7 @@ void minibufmsg(char *s) {
     int cy, cx, y, x, rlen;
     char ruler[RULER_SIZE];
     snprintf(ruler, RULER_SIZE,
-        " %luch    ", strlen(document));
+        " %luch    ", document.char_count);
     rlen = strlen(ruler);
     getyx(stdscr, cy, cx);
     getmaxyx(stdscr, y, x);
