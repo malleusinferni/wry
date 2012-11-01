852477aaf77a0da692b44722fdff9afe19445691
Mako <jlauve@rsmw.net>
2012-11-01 18:19:42 -0400

    Move function bodies

--- wry.c
+++ wry.c
@@ -46,6 +46,13 @@ int main(int argc, char **argv) {
     return 0;
 }
 
+void read_file(FILE *h) {
+    char c;
+    while ((c = getc(h)) != EOF) {
+        append_buf(c);
+    }
+}
+
 void minibufmsg(char *s) {
     int cy, cx, y, x, rlen;
     char ruler[BUFF_SIZE];
@@ -63,5 +70,101 @@ void minibufmsg(char *s) {
         mvprintw(y - 1, x - rlen, "%s", ruler);
     attroff(A_BOLD);
     move(cy, cx);
+}
+
+void init_buf() {
+    reset_buf("");
+    buf.top = buf.bot = NULL;
+    total.chars = total.words = total.lines = 0;
+}
+
+void reset_buf(char *s) {
+    buf.i = buf.wc = buf.wbreak = buf.wbeg = 0;
+    buf.inword = FALSE;
+    if (s != NULL) {
+        int i, len = strlen(s);
+        for (i = 0; i < len; i++)
+            insert_ch(s[i]);
+    }
+}
+
+void append_buf(char c) {
+    if (c == '\n') {
+        break_at(buf.i);
+    } else {
+        insert_ch(c);
+        if (buf.i > WRAP_SIZE) {
+            char save[BUFF_SIZE] = "";
+            if (buf.inword) {
+                // TODO Handle words too long to wrap
+                strncpy(save, buf.s + buf.wbeg, buf.i - buf.wbeg);
+            }
+            break_at(buf.wbreak);
+            reset_buf(save);
+        }
+    }
+}
+
+void insert_ch(char c) {
+    if (isspace(c)) {
+        if (buf.inword) {
+            buf.wbreak = buf.i;
+            buf.inword = FALSE;
+        }
+        buf.wbeg = buf.i + 1;
+    } else if (!buf.inword) {
+        buf.inword = TRUE;
+        buf.wc++;
+    }
+    buf.s[buf.i] = c;
+    buf.i++;
+}
+
+void break_at(size_t i) {
+    buf.s[i] = '\0';
+    push_line(buf.s);
+    buf.i = 0;
+}
+
+void push_line(char *s) {
+    size_t len = strlen(s) + 1;
+    line_t *nl = malloc(sizeof(line_t) + len);
+    strncpy(nl->s, s, len);
+    nl->next = NULL;
+    if (buf.top) {
+        buf.bot->next = nl;
+        buf.bot = nl;
+    } else {
+        buf.top = nl;
+        buf.bot = nl;
+    }
+}
+
+void shift_line() {
+    line_t *temp = buf.top;
+    buf.top = temp->next;
+    free(temp);
+}
+
+void drop_until(int count) {
+    int qsize = 0;
+    line_t *line = buf.top;
+    while (line) {
+        line = line->next;
+        qsize++;
+    }
+
+    while (qsize > count) {
+        shift_line();
+        qsize--;
+    }
+}
+
+void print_queue() {
+    line_t *line = buf.top;
+    while (line) {
+        puts(line->s);
+        line = line->next;
+    }
 }
 
